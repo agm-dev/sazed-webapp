eb566adcefd9114ef6aa97b76c31615dc83a632d
Adrian Gonzalo <gonzahh@gmail.com>
2020-02-05 03:53:02 +0100

    refactor actions with a better pattern

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,25 +2,19 @@ import types from "./action.types";
 import { storage } from "../config/vars";
 import { set } from "../services/storage";
 
-export const setConnection = payload => {
-  set(storage.apiBaseUrlKey, payload);
-  return { type: types.CONNECT_TO_API, payload };
-};
-
-export const setConnected = payload => ({ type: types.CONNECTED, payload });
-
-export const setAccessToken = payload => {
-  set(storage.accessTokenKey, payload);
-  return { type: types.ACCESS_TOKEN, payload }
-};
-
-export const setAuthenticated = payload => ({ type: types.AUTHENTICATED, payload });
-
-export const setUser = payload => ({ type: types.USER , payload });
-
 const setBeforeAction = (key, type) => payload => {
   set(key, payload);
   return { type, payload };
 }
+
+export const setConnection = setBeforeAction(storage.apiBaseUrlKey, types.CONNECT_TO_API);
+
+export const setConnected = payload => ({ type: types.CONNECTED, payload });
+
+export const setAccessToken = setBeforeAction(storage.accessTokenKey, types.ACCESS_TOKEN);
+
+export const setAuthenticated = payload => ({ type: types.AUTHENTICATED, payload });
+
+export const setUser = payload => ({ type: types.USER , payload });
 
 export const setCustomers = setBeforeAction(storage.customersKey, types.CUSTOMERS);
